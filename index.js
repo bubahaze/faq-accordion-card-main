362f20dc34dc904bc2dfc2655c7b6dd7d4ef09b0
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2021-07-21 19:54:01 +0200

    accordion needs collapsible feature

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 // klikniecie strzalki powoduje odkrycie odpowiedzi
 
-$(".arrow").click(() => {
-    $("p").toggleClass("pressed");
-})
+
+//     var x = document.getElementById("myDIV");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+
+
+let numberOfQuestions = $(".question").length;
+
+for (let i = 0; i < numberOfQuestions; i++) {
+
+    // //     if ($(this).hasClass("show-answer")) {
+    // //         document.querySelectorAll(".question")[i].classList.remove("show-answer");
+    // //     }
+    let content = document.querySelectorAll(".question")[i];
+    content.addEventListener("click", function() {
+        // if (content.)
+        document.querySelector("#question" + i).classList.toggle("show-answer");
+
+    });
+
+    //     //$(".question:eq(i)").toggleClass("show-answer");
+}
+
+// $(".question").click(() => {
+//     $(".question").toggleClass("show-answer");
+//     //$(".arrow").toggleClass("transform", "rotate(180deg)");
+// })
