4f5473bda04c75b6c013d143ea030efd90a3139e
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2021-07-22 18:54:54 +0200

    fix accordion functionality

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 // klikniecie strzalki powoduje odkrycie odpowiedzi
 
+this.addEventListener("DOMContentLoaded", () => {
+        const questions = document.querySelectorAll(".question")
+
+        questions.forEach((question) =>
+            question.addEventListener("click", () => {
+                if (question.parentNode.classList.contains("show-answer")) {
+                    question.parentNode.classList.toggle("show-answer")
+                } else {
+                    document.querySelectorAll(".question").forEach(question => question.parentNode.classList.remove("show-answer"))
+                    question.parentNode.classList.add("show-answer")
+                }
+            }))
+
+    })
+    // let radios = document.getElementsByTagName("input");
+    // for (let i = 0; i < radios.length; i++) {
+    //     radios[i].addEventListener("click", function(e) {
+    //             if (e.ctrlKey) {
+    //                 this.checked = false;
+    //             }
+    //         }
+    //     }
+
+
+
+
+
+
 
 //     var x = document.getElementById("myDIV");
 //   if (x.style.display === "none") {
@@ -9,22 +37,31 @@
 //   }
 
 
-let numberOfQuestions = $(".question").length;
+// let numberOfQuestions = $(".question").length;
 
-for (let i = 0; i < numberOfQuestions; i++) {
+// for (let i = 0; i < numberOfQuestions; i++) {
 
-    // //     if ($(this).hasClass("show-answer")) {
-    // //         document.querySelectorAll(".question")[i].classList.remove("show-answer");
-    // //     }
-    let content = document.querySelectorAll(".question")[i];
-    content.addEventListener("click", function() {
-        // if (content.)
-        document.querySelector("#question" + i).classList.toggle("show-answer");
+//     // //     if ($(this).hasClass("show-answer")) {
+//     // //         document.querySelectorAll(".question")[i].classList.remove("show-answer");
+//     // //     }
+//     if (document.querySelector("#question" + i).classList.contains("show-answer")) {
+//         this.classList.remove("show-answer");
+//     } else {
+//         return;
+//     }
 
-    });
+//     document.querySelectorAll(".question")[i].addEventListener("click", function() {
 
-    //     //$(".question:eq(i)").toggleClass("show-answer");
-}
+//             document.querySelector("#question" + i).classList.toggle("show-answer");
+//             //this.nextElementSibling.classList.toggle("show-answer");
+
+//         })
+//         //document.querySelector("#question" + i).classList.toggle("show-answer");
+
+// }
+
+//     //$(".question:eq(i)").toggleClass("show-answer");
+
 
 // $(".question").click(() => {
 //     $(".question").toggleClass("show-answer");
